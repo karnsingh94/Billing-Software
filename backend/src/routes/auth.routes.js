1de859afb25ef5ecb1c64d05a381b1bb3458bc0c
manishkumarsingh8 <manishkumarsingh8519@gmail.com>
2026-07-08 12:44:53 +0530

    mini change

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,14 +2,8 @@ import { Router } from "express";
 
 import { signup, login, createAdmin , createUser , logout , getMe} from "../controllers/auth.controller.js";
 import { isAuth, isSuperAdmin, isAdmin} from "../middleware/auth.middleware.js"
-
-
-
-
+//dskfjhsdkfdjfh
 const router = Router()
-
-
-
 router.post("/signup",signup );
 router.post("/login", login);
 router.post("/create-admin", isAuth, isSuperAdmin, createAdmin);
