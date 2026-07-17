947b731435aff8b13b0cd129ddb841c7a54a44d9
KARN SINGH <112151karn112151@gmail.com>
2026-07-17 16:45:37 +0530

    Resolve merge conflicts

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,18 +1,14 @@
 import { Router } from "express";
 
-<<<<<<< HEAD
-import { signup, updatePassword,  login, createAdmin , createUser , logout , getMe} from "../controllers/auth.controller.js";
-import { isAuth, isSuperAdmin, isAdmin} from "../middleware/auth.middleware.js"
-=======
 import {
   signup,
+  updatePassword,
   login,
   createAdmin,
   createUser,
   logout,
   getMe,
 } from "../controllers/auth.controller.js";
->>>>>>> 3b53bc7 (feat: add period report generation functionality with date range filtering)
 
 import {
   signupSchema,
@@ -27,27 +23,21 @@ import {
   isAdmin,
 } from "../middleware/auth.middleware.js";
 
-import {
-  validate,
-} from "../middleware/validate.middleware.js";
+import { validate } from "../middleware/validate.middleware.js";
 
 const router = Router();
 
-<<<<<<< HEAD
-router.post("/signup",signup );
-router.put("/update-password", isAuth, updatePassword);
-router.post("/login", login);
-router.post("/create-admin", isAuth, isSuperAdmin, createAdmin);
-router.post("/create-user", isAuth, isAdmin, createUser);
-router.post("/logout", isAuth, logout);
-router.get("/me", isAuth, getMe);
-=======
 router.post(
   "/signup",
   validate(signupSchema),
   signup
 );
->>>>>>> 3b53bc7 (feat: add period report generation functionality with date range filtering)
+
+router.put(
+  "/update-password",
+  isAuth,
+  updatePassword
+);
 
 router.post(
   "/login",
